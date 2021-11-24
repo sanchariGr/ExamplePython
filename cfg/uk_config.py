4f692424e64bc9a8ef4808108c87d774b412a311
Sanchari <sanchari@relayr.io>
2021-11-24 17:59:54 +0100

    Update uk_config.py

--- cfg/uk_config.py
+++ cfg/uk_config.py
@@ -3,8 +3,8 @@ import os
 # Use SELENIUM_GRID = None when you want to run the tests in a browser installed on your local machine
 SELENIUM_GRID = None
 
-# UK Vinted Url
-WEBSITE_URL = 'https://www.vinted.co.uk'
+# Url
+WEBSITE_URL = ''
 BRAND_FILTER= 'Brand'
 PRICE_FILTER= 'Price'
 FILTER_RESULT= ['From £20.00', 'To £50.00']
